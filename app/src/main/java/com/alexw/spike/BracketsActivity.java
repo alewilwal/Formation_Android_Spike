9a1551ee63750f224fe773db531f338f7812a8dd
alexwalter <alewilwal@gmail.com>
2016-09-28 13:27:13 +0200

    UI Initialisation

---
+++ app/src/main/java/com/alexw/spike/BracketsActivity.java
@@ -0,0 +1,13 @@
+package com.alexw.spike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BracketsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brackets);
+    }
+}
--- app/src/main/java/com/alexw/spike/MainActivity.java
+++ app/src/main/java/com/alexw/spike/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alexw.spike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openPalindrome(View view) {
+        Intent intent = new Intent(this, PalindromeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBrackets(View view) {
+        Intent intent = new Intent(this, BracketsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFibonacci(View view) {
+        Intent intent = new Intent(this, FibonacciActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRandomText(View view) {
+        Intent intent = new Intent(this, RandomTextActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
