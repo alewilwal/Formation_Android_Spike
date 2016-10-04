3f9a52408a4a227d4da09886e92833484c675c98
alexwalter <alewilwal@gmail.com>
2016-10-04 11:52:48 +0200

    End of Palindrome

--- app/src/main/java/com/alexw/spike/PalindromeActivity.java
+++ app/src/main/java/com/alexw/spike/PalindromeActivity.java
@@ -1,5 +1,8 @@
 package com.alexw.spike;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -19,6 +22,12 @@ public class PalindromeActivity extends AppCompatActivity {
         final EditText editText = (EditText) findViewById(R.id.edit_text);
         final TextView textView0 = (TextView) findViewById(R.id.text0);
 
+        final FloatingActionButton myFabView = (FloatingActionButton) findViewById(R.id.myFAB);
+        myFabView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View myView) {
+
+            }
+        });
 
 
         editText.addTextChangedListener(new TextWatcher() {
@@ -29,14 +38,16 @@ public class PalindromeActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                textView0.setText(editText.getText().toString());
-/*
+                /* Reverse the word */
+                textView0.setText(new StringBuilder(editText.getText().toString()).reverse().toString());
+
+                /* Change FAB color is is Palindrome */
                 if(Palindrome.isPalindrome(editText.getText().toString())){
-
+                    myFabView.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#4CAF50")));
                 }else{
-
+                    myFabView.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF5252")));
                 }
-*/
+
             }
 
             @Override
